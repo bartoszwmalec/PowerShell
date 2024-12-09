4e42584c63709fed7fd13f0c076dbd505ac4d719
Bartosz Malec <bartosz.malec@yahoo.pl>
2024-12-09 13:37:19 +0100

    adding Password generator

---
+++ password generator.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [switch]$l, # małe litery
+    [switch]$u, # duże litery
+    [switch]$d, # cyfry
+    [switch]$s, # znaki specjalne
+    [int]$c = 8 # długość hasła (domyślnie 8)
+)
+
+function New-Password {
+    param (
+        [switch]$l,
+        [switch]$u,
+        [switch]$d,
+        [switch]$s,
+        [int]$c
+    )
+
+    $pool = @()
+    if ($l) { $pool += [char[]](97..122) }
+    if ($u) { $pool += [char[]](65..90) }
+    if ($d) { $pool += [char[]](48..57) }
+    if ($s) { $pool += [char[]](33, 35, 36) }
+
+    if (-not $pool) { throw "Wybierz przynajmniej jeden typ znaków." }
+
+    -join (1..$c | ForEach-Object { $pool | Get-Random -Count 1 })
+}
+
+New-Password -l:$l -u:$u -d:$d -s:$s -c:$c
--- win10cleanup.ps1
+++ win10cleanup.ps1
@@ -1,4 +1,141 @@
-﻿Function Cleanup { 
+﻿function Get-BrowserProfile {
+    <#
+    .SYNOPSIS
+        Query Browser Profiles
+    .DESCRIPTION
+        Query Browser profiles, returning ID, Name and UserName. For current user or all users
+    .PARAMETER Browser
+        Optional. Browser app to target for query (if installed)
+        * Default = Get default browser from registry query
+        * Chrome
+        * Edge
+        * Brave
+        * Firefox
+    .PARAMETER AllUsers
+        Query all users on the computer
+    .EXAMPLE
+        Get-BrowserProfile
+    .EXAMPLE
+        Get-BrowserProfile -Browser Edge
+    .EXAMPLE
+        Get-BrowserProfile -Browser Chrome -AllUsers
+    .LINK
+        https://github.com/Skatterbrainz/helium/blob/master/docs/Get-BrowserProfile.md
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$False)][string][ValidateSet('Chrome','Edge','Brave','Firefox','Default')]$Browser = 'Default',
+        [parameter(Mandatory=$False)][switch]$AllUsers
+    )
+    if (!$AllUsers) {
+        if ($Browser -eq 'Default') {
+            $app = Get-DefaultBrowser
+        } else {
+            $app = $Browser
+        }
+        switch ($app) {
+            'Chrome' {
+                $profilePath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
+            }
+            'Brave' {
+                $profilePath = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data"
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
+            }
+            'Edge' {
+                $profilePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data"
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
+            }
+            'Firefox' {
+                $profilePath = "$env:APPDATA\Mozilla\Firefox\Profiles"
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory | select-object Name,FullName
+            }
+        }
+        if ($app -ne 'Firefox') {
+            foreach ($folder in $profileFolders) {
+                [string]$pref = Join-Path $folder.FullName "Preferences"
+                if (Test-Path $pref) {
+                    $prefdata = Get-Content $pref -Raw | ConvertFrom-Json
+                    $name = $prefdata.profile.name
+                    [pscustomobject]@{
+                        UserName    = $env:USERNAME
+                        ProfileID   = $folder.Name
+                        ProfileName = $name
+                        Browser     = $app
+                    }
+                }
+            }
+        } else {
+            foreach ($folder in $profileFolders) {
+                $prefdata = $folder.Name.Split('.')
+                $name = $prefdata[1]
+                [pscustomobject]@{
+                    UserName    = $env:USERNAME
+                    ProfileID   = $folder.Name
+                    ProfileName = $name
+                    Browser     = $app
+                }
+            }
+        }
+    } else {
+        $exclude = @('AppData','Public')
+        if ($Browser -eq 'Default') {
+            $app = Get-DefaultBrowser
+        } else {
+            $app = $Browser
+        }
+        switch ($app) {
+            'Chrome' {
+                $profileSub = "AppData\Google\Chrome\User Data"
+            }
+            'Brave' {
+                $profileSub = "AppData\Local\BraveSoftware\Brave-Browser\User Data"
+            }
+            'Edge' {
+                $profileSub = "AppData\Local\Microsoft\Edge\User Data"
+            }
+            'Firefox' {
+                $profileSub = 'AppData\Local\Mozilla\Firefox\Profiles'
+            }
+        }
+        [array]$userprofiles = Get-ChildItem -Path "c:\users" -Directory | Where-Object {$_.name -notin $exclude} | Select-Object Name,FullName
+        Write-Verbose "$($userprofiles.Count) user profiles found"
+        foreach ($userpath in $userprofiles) {
+            $profilePath = Join-Path $($userpath.FullName) $profileSub
+            Write-Verbose "reading: $profilePath"
+            if ($app -ne 'Firefox') {
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
+                foreach ($folder in $profileFolders) {
+                    [string]$pref = Join-Path $folder.FullName "Preferences"
+                    if (Test-Path $pref) {
+                        $prefdata = Get-Content $pref -Raw | ConvertFrom-Json
+                        $name = $prefdata.profile.name
+                        [pscustomobject]@{
+                            UserName    = $userpath.Name
+                            ProfileID   = $folder.Name
+                            ProfileName = $name
+                            Browser     = $app
+                        }
+                    }
+                }
+            } else {
+                [array]$profileFolders = Get-ChildItem -Path $profilePath -Directory | select-object Name,FullName
+                foreach ($folder in $profileFolders) {
+                    $prefdata = $folder.Name.Split('.')
+                    $name = $prefdata[1]
+                    [pscustomobject]@{
+                        UserName    = $env:USERNAME
+                        ProfileID   = $folder.Name
+                        ProfileName = $name
+                        Browser     = $app
+                    }
+                }
+            }
+        }
+    }
+}
+
+Function Cleanup { 
 <# 
 .CREATED BY: 
     Matthew A. Kerfoot 
@@ -65,6 +202,40 @@ Get-ChildItem "C:\users\*\AppData\Local\Microsoft\Windows\Temporary Internet Fil
 Where-Object {($_.CreationTime -le $(Get-Date).AddDays(-$DaysToDelete))} | 
 remove-item -force -recurse -ErrorAction SilentlyContinue 
 
+## Delets all files and folders in user's Temp folder.  
+#Get-ChildItem "$env:LOCALAPPDATA\Google\Chrome\User Data\*" -Recurse -Force -ErrorAction SilentlyContinue | 
+#Where-Object { ($_.CreationTime -lt $(Get-Date).AddDays(-$DaysToDelete))} | 
+#remove-item -force -Verbose -recurse -ErrorAction SilentlyContinue 
+
+## Part for Chrome temp folders
+$ChromeProfileNames = @(
+"Profile 1",                                                                                            
+"Profile 3",                                                                                            
+"Profile 6",                                                                                            
+"Profile 7",                                                                                            
+"Profile 8"
+<#,
+"Default" #>
+)
+
+$ChromeProfileFolder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\"
+$ChromeProfileNames | ForEach-Object { 
+    if (Test-Path "$ChromeProfileFolder\$_") {
+            Get-ChildItem -Path "$ChromeProfileFolder\$_" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Cache\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Archived History\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\History\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Top Sites\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Visited Links\*" | Where-Object { $_.Name -notlike "*Bookmarks*" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Web Data\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Cache\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Cache2\entries\*" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Cookies" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Media Cache" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+            Get-ChildItem -path "$ChromeProfileFolder\$_\Cookies-Journal" | Where-Object { $_.Name -ne "Bookmarks" } | Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue
+    }
+    }
+
 ## Remove all files and folders in user's INetCache.  
 Get-ChildItem "C:\users\*\AppData\Local\Microsoft\Windows\INetCache\*" `
 -Recurse -Force -Verbose -ErrorAction SilentlyContinue | 
@@ -75,13 +246,14 @@ remove-item -force -recurse -ErrorAction SilentlyContinue
 ## All Temporary Internet Files have been removed successfully! 
 ## Cleans IIS Logs if applicable. 
 Get-ChildItem "C:\inetpub\logs\LogFiles\*" -Recurse -Force -ErrorAction SilentlyContinue | 
-Where-Object { ($_.CreationTime -le $(Get-Date).AddDays(-60)) } | 
+Where-Object { ($_.CreationTime -le $(Get-Date).AddDays(-$DaysToDelete)) } | 
 Remove-Item -Force -Verbose -Recurse -ErrorAction SilentlyContinue 
 ## All IIS Logfiles over x days old have been removed Successfully! 
 ## deletes the contents of the recycling Bin. 
 ## The Recycling Bin is now being emptied! 
 $objFolder.items() | ForEach-Object { Remove-Item $_.path -ErrorAction Ignore -Force -Verbose -Recurse } 
 ## The Recycling Bin has been emptied! 
+<#
 ## Clean up Chrome files
 $Items = @('Archived History',
             'Cache\*',
@@ -97,6 +269,8 @@ $Items | ForEach-Object {
         Remove-Item "$Folder\$_" -Force -Verbose -Recurse -ErrorAction SilentlyContinue 
     }
 }
+#>
+
 ##
 $After =  Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName, 
 @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } }, 
@@ -116,6 +290,8 @@ Get-Service -Name wuauserv | Start-Service -Verbose -ErrorAction SilentlyContinu
 }
 $currentDate = Get-Date
 Write-Host "Current date and time: $currentDate"
+
+#Get-BrowserProfile
 
 Cleanup
 
